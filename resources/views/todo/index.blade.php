b35e697b5b2a897523cdd2d6b646c878e083555f
Ubuntu <ubuntu@ip-172-31-43-222.us-west-2.compute.internal>
2021-01-07 06:13:04 +0000

    update title

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Todo Index') }}
+      {{ __('Todos Index') }}
     </h2>
   </x-slot>
 
